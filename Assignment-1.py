5370317ee3e379f35bde41a7e690ae13968f6d04
Abinashchy <54818124+Abinashchy@users.noreply.github.com>
2020-04-20 14:35:39 +0545

    Add files via upload

---
+++ Assignment-1.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Name:Abinash Chaudhary
+Roll:11172655
+Sec-C3    
+
+
+# In[1]:
+
+
+#Write a Python program to insert an element (specific position) into an
+#List.
+
+l1=[2,3,4,5,6,7]
+l1.insert(3,25)
+print(l1)
+
+
+# In[3]:
+
+
+# Write a Python program to sort a numeric List and a string List.
+l2=[5,3,9,6,8,1]
+l2.sort()
+print(l2)
+
+
+# In[5]:
+
+
+l3=["Python","Java","Android","Cloud"]
+l3.sort()
+print(l3)
+
+
+# In[10]:
+
+
+# Write a Python program to reverse a List of integer values.
+l4=[4,2,6,8,1,9,5]
+l5=l4[::-1]
+print(l5)
+
+
+# In[11]:
+
+
+#Write a Python program to find the common elements between two List
+#(string values).
+
+l6=["Python","Java","Android","Cloud"]
+l7=["Java","Big Data","Ds","Math"]
+print(set(l6) & set(l7))
+
+
+# In[17]:
+
+
+#Write a Python program to find the common elements between two Listt
+#of integers.
+l8=[2,4,6,8,10]
+l9=[1,4,9,16]
+print(set(l8) & set(l9))
+
+
+# In[ ]:
+
+
+
+
